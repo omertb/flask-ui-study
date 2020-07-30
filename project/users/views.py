487526653d2ddafe3b03593c16a1668d2599ff36
omertb <omertugrul@gmail.com>
2020-07-30 15:04:19 +0300

    wtf integrated to login page

--- project/users/views.py
+++ project/users/views.py
@@ -1,5 +1,7 @@
 from flask import flash, redirect, render_template, url_for, session, request, Blueprint
 from functools import wraps
+from project.users.forms import LoginForm
+
 
 users_blueprint = Blueprint('users', __name__, template_folder='templates')
 
@@ -19,16 +21,20 @@ def login_required(f):
 # route for handling the login page logic
 @users_blueprint.route('/login', methods=['GET', 'POST'])
 def login():
+    form = LoginForm(request.form)
     error = None
     if request.method == 'POST':
-        if (request.form['username'] != 'admin') \
-                or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
+        if form.validate_on_submit():
+            if (request.form['username'] != 'admin') \
+                    or request.form['password'] != 'admin':
+                error = 'Invalid Credentials. Please try again.'
+            else:
+                session['logged_in'] = True
+                flash('You are logged in.')
+                return redirect(url_for('home.home'))
         else:
-            session['logged_in'] = True
-            flash('You are logged in.')
-            return redirect(url_for('home.home'))
-    return render_template('login.html', error=error)
+            return render_template('login.html', form=form, error=error)
+    return render_template('login.html', form=form, error=error)
 
 
 @users_blueprint.route('/logout')
